d252b3669f2b863a93810ec2fa31e84f3364b439
rbhasarr <31952082+rbhasarr@users.noreply.github.com>
2019-05-20 18:58:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,12 @@ var response1 = '';
 var venuecity = '';
 var venue = '';
 var newresponse = '';
-var Port= 8080;
-var i = 0;
+var Port = 8080;
 var request = require('request');
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.get('/',urlencodedParser,function(req,res){
-    var request = require("request");
+    request = require("request");
     
     var options = { method: 'GET',
       url: 'https://api.meetup.com/pro/qlik/events',
@@ -27,36 +26,36 @@ app.get('/',urlencodedParser,function(req,res){
       response1 = body;
       var jsondata = JSON.parse(response1);
       jsondata = jsondata.sort(function(a, b){
-      var date1 = new Date(a.event.local_date);
-      date1 = date1.getTime();
-      var date2 = new Date(b.event.local_date);
-      date2 = date2.getTime();
-      return date1 - date2;
-    });
-      jsondata.forEach(function (json) {        
-        if(i < 5){
-          const date = new Date(json.event.local_date);  // 2009-11-10
-          const month = date.toLocaleString('en-us', { month: 'short' });
-          const dateNumber = date.getDate();
-          if(typeof json.event.group === "undefined" || json.event.group === null){
-            venuecity = '';
-            venue = '';
-          }
-          else{
-            venuecity = 'At ' + json.event.group.localized_location;
-             venue = json.event.group.localized_location;
-          }
-          newresponse = newresponse + '<li class="meetups-data" data-location="' + venue + '"><div class="event-details"><div class="date-holder"><div class="date"><span class="date-text">' + dateNumber + '</span></div><div class="month"><span class="month-text">' + month + '</span></div></div><div class="event-description"><h4><a href="' + json.event.link + '" target="_blank">' + json.event.name + '</a></h4><span class="event-place">' + venuecity + '</span><span class="event-time">' + json.event.local_time + '</span></div></div></li>';
-          i++;
-        }
+				var date1 = new Date(a.event.local_date);
+				date1 = date1.getTime();
+				var date2 = new Date(b.event.local_date);
+				date2 = date2.getTime();
+				return date1 - date2;
+			});
+			var i = 0;
+      jsondata.forEach(function (json) {
+				if (i < 5){
+					const date = new Date(json.event.local_date);  // 2009-11-10
+					const month = date.toLocaleString('en-us', { month: 'short' });
+					const dateNumber = date.getDate();
+					if(typeof json.event.group === "undefined" || json.event.group === null){
+						venuecity = '';
+						venue = '';
+					}
+					else{
+						venuecity = 'At ' + json.event.group.localized_location;
+						venue = json.event.group.localized_location;
+					}
+					newresponse = newresponse + '<li class="meetups-data" data-location="' + venue + '"><div class="event-details"><div class="date-holder"><div class="date"><span class="date-text">' + dateNumber + '</span></div><div class="month"><span class="month-text">' + month + '</span></div></div><div class="event-description"><h4><a href="' + json.event.link + '" target="_blank">' + json.event.name + '</a></h4><span class="event-place">' + venuecity + '</span><span class="event-time">' + json.event.local_time + '</span></div></div></li>';
+					i++;
+				}
       });
       newresponse = newresponse + '<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBYvZL9yBnDq79sxZ4RdZu-b9XzQGTUrIs&callback=initMap"></script>';
       res.send(newresponse);
-      
+			newresponse = '';
       res.status(200).end();
-        newresponse = '';
     });
 });
 app.listen(Port, function () {
-    console.log("Server is listening on port: " +Port);
+    console.log("Server is listening on port: " + Port);
 });
